6b17c2b473a18bcd44a910d8ed253f8922273a27
kleo4004 <zkioz4004@gmail.com>
2020-06-08 23:10:06 +0300

    feat:add modal window

--- src/js/script.js
+++ src/js/script.js
@@ -32,5 +32,18 @@ $(document).ready(function () {
     
     toggleDescription('.catalog-item__link');
     toggleDescription('.catalog-item__back');
- 
+    
+    //modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
